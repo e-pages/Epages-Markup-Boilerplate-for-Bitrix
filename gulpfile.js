9e3acf111900797d26f1eac4b31406e1b5a36c2b
Vadim <Rod-rig@users.noreply.github.com>
2016-01-30 23:44:39 +0200

    add bower components add devDependencies rewrite gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,12 @@ var concat = require('gulp-concat');
 var mainLessFile = './less/styles.less';
 var testLessFile = './less/test/test.less';
 var fontFaceLessFile = './fonts/font-faces.less';
-var jsLibs = ['./js/libs/jquery-2.1.4.min.js', './js/libs/holder.min.js', './js/libs/bootstrap.min.js'];
+var mainBootstrapFilePath = 'bower_components/bootstrap/less/bootstrap.less';
+var mainFontAwesomeFilePath = 'bower_components/font-awesome/less/font-awesome.less';
+var jqueryFilePath = 'bower_components/jquery/dist/jquery.min.js';
 
 //compile main less file & compress it
-gulp.task('compile_Main_Less_File', function () {
+gulp.task('compileMainLessFile', function () {
     return gulp.src(mainLessFile)
         .pipe(less())
         .pipe(csso())
@@ -19,7 +21,7 @@ gulp.task('compile_Main_Less_File', function () {
 });
 
 //compile test less file & compress it
-gulp.task('compile_Test_Less_File', function () {
+gulp.task('compileTestLessFile', function () {
     return gulp.src(testLessFile)
         .pipe(less())
         .pipe(csso())
@@ -27,12 +29,18 @@ gulp.task('compile_Test_Less_File', function () {
 });
 
 //compile font-face less file & compress it
-gulp.task('compile_Font_Face_File', function () {
+gulp.task('compileFontFaceFile', function () {
     return gulp.src(fontFaceLessFile)
         .pipe(less())
         .pipe(csso())
         .pipe(gulp.dest('./css'));
 });
+
+
+
+//var jsLibs = ['./js/libs/jquery-2.1.4.min.js', './js/libs/holder.min.js', './js/libs/bootstrap.min.js'];
+
+
 
 //minify js
 //gulp.task('minify_js', function () {
@@ -42,28 +50,28 @@ gulp.task('compile_Font_Face_File', function () {
 //});
 
 //concat js
-gulp.task('concat_js', function() {
-    return gulp.src(jsLibs)
-        .pipe(concat('libs.min.js'))
-        .pipe(gulp.dest('./js'));
-});
+//gulp.task('concat_js', function() {
+//    return gulp.src(jsLibs)
+//        .pipe(concat('libs.min.js'))
+//        .pipe(gulp.dest('./js'));
+//});
 
 gulp.task('default', function () {
-    gulp.run('concat_js');
+    //gulp.run('concat_js');
 
     //watch for styles.less
     gulp.watch(mainLessFile, function () {
-        gulp.run('compile_Main_Less_File');
+        gulp.run('compileMainLessFile');
     });
 
     //watch for test.less
     gulp.watch(testLessFile, function () {
-        gulp.run('compile_Test_Less_File');
+        gulp.run('compileTestLessFile');
     });
 
     //watch for test.less
     gulp.watch(fontFaceLessFile, function () {
-        gulp.run('compile_Font_Face_File');
+        gulp.run('compileFontFaceFile');
     });
 
     //gulp.watch('./js/script.js', function () {
