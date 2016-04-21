8848975ecec567ba5eb5f3194a7028889132ebc7
Rod-rig <cherniak-vadim@ro.ru>
2016-04-22 00:04:35 +0300

    fixed styles for nested menus

--- components/nav/menu.tree.php
+++ components/nav/menu.tree.php
@@ -5,44 +5,67 @@
  */
 ?>
 <nav class="<?=$arParams['MODIFIER']?>">
-    <ul>
+    <ul class="list-group list-unstyled">
         <?php foreach ($arResult as $level1) :?>
-            <li>
-                <a <?php if (isset($level1['CHILDREN'])) :?>class="active" data-toggle="collapse"
-                   href="#<?= $level1['ID'] ?>" aria-expanded="false" aria-controls="<?= $level1['ID'] ?>"
-                    <?php else : ?>href="<?= $level1['LINK']?>"<?php endif; ?>>
-                    <?= $level1['TEXT']?>
-                    <?php if (isset($level1['CHILDREN'])) :?><span class="caret"><?php endif; ?></span>
+            <li class="list-group-item">
+                <a href="<?php echo $level1['LINK']?>">
+                    <?= $level1['TEXT'] ?>
                 </a>
+                <?php if (isset($level1['CHILDREN'])) : ?>
+                    <span class="badge" data-toggle="collapse"
+                          data-target="#<?= $level1['ID'] ?>"
+                          aria-expanded="false"
+                          aria-controls="<?= $level1['ID'] ?>">
+                            <span class="fa fa-caret-down"></span>
+                    </span>
+                <?php endif; ?>
                 <?php if (isset($level1['CHILDREN'])) :?>
-                    <ul <?php if (isset($level1['CHILDREN'])) :?>id="<?= $level1['ID']?>" class="collapse"<?php endif; ?>>
+                    <ul class="list-group list-unstyled collapse" <?php if (isset($level1['CHILDREN'])) :?>id="<?= $level1['ID']?>"<?php endif; ?>>
                         <?php foreach ($level1['CHILDREN'] as $level2) :?>
-                            <li>
-                                <a <?php if (isset($level2['CHILDREN'])) :?>class="active" data-toggle="collapse"
-                                   href="#<?= $level2['ID'] ?>" aria-expanded="false" aria-controls="<?= $level2['ID'] ?>"
-                                   <?php else : ?>href="<?= $level2['LINK']?>"<?php endif; ?>>
-                                    <?= $level2['TEXT']?>
-                                    <?php if (isset($level2['CHILDREN'])) :?><span class="caret"><?php endif; ?></span>
+                            <li class="list-group-item">
+                                <a href="<?php echo $level2['LINK']?>">
+                                    <?= $level2['TEXT'] ?>
                                 </a>
+                                <?php if (isset($level2['CHILDREN'])) : ?>
+                                    <span class="badge" data-toggle="collapse"
+                                          data-target="#<?= $level2['ID'] ?>"
+                                          aria-expanded="false"
+                                          aria-controls="<?= $level2['ID'] ?>">
+                                            <span class="fa fa-caret-down"></span>
+                                    </span>
+                                <?php endif; ?>
                                 <?php if (isset($level2['CHILDREN'])) :?>
-                                    <ul <?php if (isset($level2['CHILDREN'])) :?>id="<?= $level2['ID']?>"
-                                        class="collapse"<?php endif; ?>>
+                                    <ul class="list-group list-unstyled collapse"
+                                        <?php if (isset($level2['CHILDREN'])) :?>id="<?= $level2['ID']?>"<?php endif; ?>>
                                         <?php foreach ($level2['CHILDREN'] as $level3) :?>
-                                            <li>
-                                                <a <?php if (isset($level3['CHILDREN'])) :?>class="active" data-toggle="collapse"
-                                                   href="#<?= $level3['ID'] ?>" aria-expanded="false" aria-controls="<?= $level3['ID'] ?>"
-                                                   <?php else : ?>href="<?= $level3['LINK']?>"<?php endif; ?>>
-                                                    <?= $level3['TEXT']?>
-                                                    <?php if (isset($level3['CHILDREN'])) :?><span class="caret"><?php endif; ?></span>
+                                            <li class="list-group-item">
+                                                <a href="<?php echo $level3['LINK']?>">
+                                                    <?= $level3['TEXT'] ?>
                                                 </a>
+                                                <?php if (isset($level3['CHILDREN'])) : ?>
+                                                    <span class="badge" data-toggle="collapse"
+                                                          data-target="#<?= $level3['ID'] ?>"
+                                                          aria-expanded="false"
+                                                          aria-controls="<?= $level3['ID'] ?>">
+                                                            <span class="fa fa-caret-down"></span>
+                                                    </span>
+                                                <?php endif; ?>
                                                 <?php if (isset($level3['CHILDREN'])) :?>
-                                                    <ul <?php if (isset($level3['CHILDREN'])) :?>id="<?= $level3['ID']?>"
-                                                        class="collapse"<?php endif; ?>>
+                                                    <ul class="list-group list-unstyled collapse"
+                                                        <?php if (isset($level3['CHILDREN'])) :?>id="<?= $level3['ID']?>"<?php endif; ?>>
                                                         <?php foreach ($level3['CHILDREN'] as $level4) :?>
-                                                            <li>
-                                                                <a href="<?=$level4['LINK']?>"
-                                                                    <?=($level4['SELECTED']) ? ' class="active"' : ''?>
-                                                                ><?=$level4['TEXT']?></a>
+                                                            <li class="list-group-item">
+                                                                <a href="<?php echo $level4['LINK']?>">
+                                                                    <?= $level4['TEXT'] ?>
+                                                                </a>
+                                                                <?php if (isset($level4['CHILDREN'])) : ?>
+                                                                    <span class="badge" data-toggle="collapse"
+                                                                          data-target="#<?= $level4['ID'] ?>"
+                                                                          aria-expanded="false"
+                                                                          aria-controls="<?= $level4['ID'] ?>">
+                                                                            <span class="fa fa-caret-down"></span>
+                                                                    </span>
+                                                                <?php endif; ?>
                                                             </li>
                                                         <?php endforeach;?>
                                                     </ul>
