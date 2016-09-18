47a482e7958081c0aa67b29bd79e802ee473d88b
Anton Lee <antonlee0@gmail.com>
2016-09-18 14:12:58 +0300

    restore local.js

---
+++ js/local.js
@@ -0,0 +1,20 @@
+/*global $, jQuery*/
+$(document).ready(function () {
+    //variables
+    var timeAnimation = 300;
+
+    //accordion
+    $('.first-level-link').click(function (e) {
+        e.preventDefault();
+        $(this).siblings('ul').slideToggle();
+        $(this).parent('li').siblings('li').children('ul').slideUp();
+    });
+
+    //scroll up
+    $('.up').click(function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            'scrollTop': 0
+        }, timeAnimation);
+    });
+});
